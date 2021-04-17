e0f42ecb8a8cbfa10a27b27c65ed6345dbd8e40f
Wojtek Kłusek <wojtek.klusek2@gmail.com>
2021-04-17 15:04:17 +0200

    add password model tests

--- README.md
+++ README.md
@@ -3,7 +3,14 @@
 ## Requirements
 - Python 3.8
 
-## install
+## Install
 ```sh
 python -m pip install -r requirements.txt
+```
+## Test
+
+Run tests
+```shell
+cd password_webapp_project
+pytest
 ```
--- password_webapp_project/password_app/tests/models_tests/test_user_model.py
+++ password_webapp_project/password_app/tests/models_tests/test_user_model.py
@@ -1,6 +1,36 @@
 from django.contrib.auth.models import User
-
+from password_app.models import Password
+from datetime import date
 
 def test_users_data_load(db, django_db_setup):
     users_list = User.objects.all()
     assert len(users_list) > 0
+
+
+def test_create_user(db, django_db_setup):
+    user = User.objects.get(first_name="Admin")
+
+    password = Password(
+        description="Test password",
+        password="Password",
+        expiration_date = date.today(),
+        password_owner=user
+    )
+    password.save()
+    password_from_db = Password.objects.get(password_owner=user)
+
+    assert password_from_db.description == "Test password"
+
+def test_password_share(db, django_db_setup):
+    user = User.objects.get(first_name="Admin")
+
+    password = Password(
+        description="Test password",
+        password="Password",
+        expiration_date=date.today(),
+        password_owner=user
+    )
+    password.save()
+    password.shared_users = [user]
+
+    assert user in password.shared_users
