cd31e6f905896717148af121db68b1e70e22f09d
Wojtek Kłusek <wojtek.klusek2@gmail.com>
2021-04-19 21:22:45 +0200

    add urls to ListView template

--- password_webapp_project/password_app/urls.py
+++ password_webapp_project/password_app/urls.py
@@ -6,7 +6,7 @@ app_name = 'password_app'
 urlpatterns = [
     path('', views.PasswordListView.as_view(), name='list'),
     path('show/<int:pk>/', views.PasswordDetailView.as_view(), name='detail'),
-    path('add/', views.PasswordDetailView.as_view(), name='detail'),
-    path('update/<int:pk>/', views.PasswordDetailView.as_view(), name='detail'),
-    path('delete/<int:pk>/', views.PasswordDetailView.as_view(), name='detail'),
+    path('add/', views.PasswordDetailView.as_view(), name='add'),
+    path('update/<int:pk>/', views.PasswordDetailView.as_view(), name='update'),
+    path('delete/<int:pk>/', views.PasswordDetailView.as_view(), name='delete'),
 ]
