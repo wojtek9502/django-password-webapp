fc738cce2b3440ab0fb0218014643f825de439fc
Wojtek Kłusek <wojtek.klusek2@gmail.com>
2021-04-19 21:01:40 +0200

    add password views and urls

---
+++ password_webapp_project/password_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'password_app'
+urlpatterns = [
+    path('', views.PasswordListView.as_view(), name='list'),
+    path('show/<int:pk>/', views.PasswordDetailView.as_view(), name='detail'),
+    path('add/', views.PasswordDetailView.as_view(), name='detail'),
+    path('update/<int:pk>/', views.PasswordDetailView.as_view(), name='detail'),
+    path('delete/<int:pk>/', views.PasswordDetailView.as_view(), name='detail'),
+]
--- password_webapp_project/password_app/views.py
+++ password_webapp_project/password_app/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import generic
 
-# Create your views here.
+from .models import Password
+
+
+class PasswordListView(generic.ListView):
+    model = Password
+    context_object_name = 'user_passwords'
+    template_name = 'password_app/password_list.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        qs = Password.objects.all()
+        return qs
+
+
+class PasswordDetailView(generic.DetailView):
+    model = Password
+    context_object_name = 'user_password'
+    template_name = 'password_app/password_detail.html'
