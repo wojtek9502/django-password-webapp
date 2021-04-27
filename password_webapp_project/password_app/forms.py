d002507833187cc22a2e30968cadbfe0604042cc
Wojtek Kłusek <wojtek.klusek2@gmail.com>
2021-04-27 19:33:56 +0200

    update password model

--- password_webapp_project/password_app/forms.py
+++ password_webapp_project/password_app/forms.py
@@ -1,10 +1,7 @@
 from django import forms
-from django.contrib.auth.models import User
-from django.db.models import Q
 from django.urls import reverse_lazy
 
 from .models import Password
-
 
 
 class PasswordCreateForm(forms.ModelForm):
