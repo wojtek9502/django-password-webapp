77081e460d363bd14e646bdfcba62154cf83055f
Wojtek Kłusek <wojtek.klusek2@gmail.com>
2021-05-06 20:13:16 +0200

    add templates and urls

--- password_webapp_project/accounts_app/urls.py
+++ password_webapp_project/accounts_app/urls.py
@@ -10,15 +10,16 @@ urlpatterns = [
     path('afterLogout/', views.AfterLogoutView.as_view(), name="afterLogout"),
     path('register/', views.RegisterView.as_view(), name='register'),
 
+    path('password_reset/', views.PasswordResetRequest.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name="accounts_app/password_reset_done.html"),
+         name='password_reset_done'),
+    path('reset_password/<uidb64>/(<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="accounts_app/password_reset_confirm.html"),
+         name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name="accounts_app/password_reset_complete.html"),
+         name='password_reset_complete'),
+
     path('account_settings/', views.AccountSettingsView.as_view(), name='account_settings'),
     path('account_change_password/', views.AccountChangePasswordView.as_view(), name='account_change_password'),
     path('account_change_email/', views.AccountChangeEmailView.as_view(), name='account_change_email'),
-    # path(r'^after_signup/$', views.AfterRegisterPage.as_view(), name="after_signup"),
-    #
-    # path(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
-    # path(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
-    # path(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-    #     auth_views.password_reset_confirm, name='password_reset_confirm'),
-    # path(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 
 ]
